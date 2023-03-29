28c7e1b0bbc3aec65b5b82e665c1faa0f5dca60d
Elijah <46640763+ghwosty@users.noreply.github.com>
2023-03-29 19:58:39 +0000

    add modules function

--- rel/requires.js
+++ rel/requires.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const fs = require('fs');
+const manager = require("./managers");
 const FgRed = "\x1b[31m"
 
-module.exports = { path, fs, FgRed }
+module.exports = { path, fs, FgRed, manager }
