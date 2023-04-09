bb221d220ea92f6ce98ce8893ad20f1c424a5aed
darthvader1925 <elijb12@gmail.com>
2023-04-09 12:37:56 -0700

    add js encoding

--- installer/encode.py
+++ installer/encode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:487768cacab947d4100149e380947bf452eb12e4018316ccfc5790ce22072022
-size 286
+oid sha256:ed3628a008823d90bbbc01c2d222998eee39c5005fbbbe33fd45c64db511e697
+size 442
