c8d73675bbcf0eb7aa78d845de214c0b69927e48
darthvader1925 <elijb12@gmail.com>
2023-06-13 13:10:01 -0700

    more stuff

--- website/downloader.js
+++ website/downloader.js
@@ -6,4 +6,17 @@ fetch("https://rel-server.onrender.com/downloads")
     .then((d) => d.text())
     .then((d) => {
         document.getElementById("downloadAmt").innerText = numberWithCommas(d);
-    });
+    })
+    .catch((err) => {
+        document.getElementById("downloadAmt").innerText = "Failed"
+        document.getElementById("downloadLabel").innerText = " to load downloads"
+    })
+
+fetch("https://rel-server.onrender.com/current-version")
+    .then((d) => d.text())
+    .then((d) => {
+        document.querySelector("#downloadBtn > span").innerHTML = "Download v" + d
+    })
+    .catch((err) => {
+        document.querySelector("#downloadBtn > span").innerHTML = "Download"
+    })
