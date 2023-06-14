ae0309ef3f617b503024de3c05257e3d7685c16c
darthvader1925 <elijb12@gmail.com>
2023-06-13 17:42:11 -0700

    update download tracking

--- docs/downloader.js
+++ docs/downloader.js
@@ -20,3 +20,28 @@ fetch("https://rel-server.onrender.com/current-version")
     .catch((err) => {
         document.querySelector("#downloadBtn > span").innerHTML = "Download"
     })
+
+document.getElementById("downloadLink").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    fetch("https://rel-server.onrender.com/update-downloads", {
+        method: "POST"
+    })
+        .then(() => {
+            window.location = "https://github.com/a-riceeater/rel/releases/latest/download/rel.exe"
+
+            fetch("https://rel-server.onrender.com/downloads")
+                .then((d) => d.text())
+                .then((d) => {
+                    document.getElementById("downloadAmt").innerText = numberWithCommas(d);
+                })
+                .catch((err) => {
+                    document.getElementById("downloadAmt").innerText = "Failed"
+                    document.getElementById("downloadLabel").innerText = " to load downloads"
+                })
+        })
+        .catch((err) => {
+            console.error(err);
+            alert("An error occured. Try again later?")
+        })
+})
