2a16ccbd7dee43acccd96a744487b254505e3d43
darthvader1925 <elijb12@gmail.com>
2023-06-15 10:34:26 -0700

    installer stuff

--- installer/installer-src.js
+++ installer/installer-src.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3f617378aeb8b4d9f171b526d2de8c1ba7e4eadf379b679d2fb048e9fe02f8d
-size 7843
+oid sha256:73bb0f946916305757df8a7d6a87ce10a4afb17516e0739b248e8035447e4ebd
+size 7838
